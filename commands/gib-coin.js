2601e3569d4831d3f258175f4682a9c4fa8b8983
T-Elsener <129606371+T-Elsener@users.noreply.github.com>
2023-04-07 12:13:20 -0400

    feat: command handler + gib-coins command

---
+++ commands/gib-coin.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'gib-coin',
+    description: 'Tests the bots ability to read messages from users',
+    execute(message,args){
+        message.channel.send('STFU,I have no sympathy for the poor!');
+    }
+}
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 //Connects disord.js API to the rest of the program
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const token =
+  "[Token Goes here]";
+const fs = require("fs");
+
+//Create a prefix that the bot will listen for in order to process commands
+const prefix = "!";
 
 //Create a new Client object and name it bot
-const bot = new Discord.Client({
-    intents: [
-        Discord.GatewayIntentBits.Guilds,
-        Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.MessageContent,
-        Discord.GatewayIntentBits.GuildMembers
-    ]
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMembers,
+  ],
 });
-const token = '[Token goes here]';
+
+//Command handler, creates collection for commands and loops through to find only .js files
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+//Checks to make sure user message contains command prefix and executes commands
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  //Checks to see if command matches the names of files in /commands and executes
+  if (command == "gib-coin") {
+    client.commands.get("gib-coin").execute(message, args);
+  }
+});
 
 //Log into the bot and pass in the token
-bot.on('ready', () => {
-    console.log(`Bot is online with user: ${bot.user.username}`);
-})
-bot.login(token);
+client.on("ready", () => {
+  console.log(`Bot is online with user: ${client.user.username}`);
+});
+
+//Log into the bot and bring online by passing token
+client.login(token);
